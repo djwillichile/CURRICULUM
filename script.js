3d4414bb35c9df48707ea88f51c4f8a4d7eb417e
djwillichile <guillermo.f1990@gmail.com>
2023-06-21 00:42:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ window.onload = function() {
         'Licencia de conducir clase B': 6
     };
 
+
     const educationDetails = [
         {
             degree: 'Magíster en Gestión Territorial de Recursos Naturales',
@@ -42,6 +43,116 @@ window.onload = function() {
             year: '2018',
         },
     ];
+
+    let cursos = [
+        {
+            nombre: "Sistemas de Información Geografica Aplicada a los Recursos Naturales Renovables",
+            organizacion: "Escuela de postgrado, Facultad de Ciencias Agronómicas, Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2021
+        },
+        {
+            nombre: "Métodos para el Análisis Económico y Social",
+            organizacion: "Escuela de postgrado, Facultad de Ciencias Agronómicas, Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2020
+        },
+        {
+            nombre: "Sistemas de Soporte a la Toma de Decisiones",
+            organizacion: "Escuela de postgrado, Facultad de Ciencias Agronómicas, Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2020
+        },
+        {
+            nombre: "Estadística Aplicada a los Recursos Naturales Renovables",
+            organizacion: "Escuela de postgrado, Facultad de Ciencias Agronómicas, Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2020
+        },
+        {
+            nombre: "Mapeo y Monitoreo de los Bosques con Datos SAR",
+            organizacion: "Programa de Capacitación de Teledetección Aplicada de la NASA (ARSET)",
+            ubicacion: "online",
+            año: 2020
+        },
+        {
+            nombre: "Biosphere - atmosphere interactions",
+            organizacion: "Postgraduate Summer School, Faculty of Agricultural Sciences, University of Chile",
+            ubicacion: "Frutillar town, Southern Chile",
+            año: 2019
+        },
+        {
+            nombre: "Evaluación de Proyectos",
+            organizacion: "Facultad de Ciencias Agronómicas, Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2018
+        },
+        {
+            nombre: "Ordenamiento Territorial",
+            organizacion: "Facultad de Ciencias Agronómicas, Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2018
+        },
+        {
+            nombre: "Micrometeorología y Contaminación Atmosférica",
+            organizacion: "Facultad de Ciencias, Universidad Nacional Autónoma de México",
+            ubicacion: "Distrito Federal, México",
+            año: 2015
+        },
+        {
+            nombre: "Aerosoles Atmosféricos",
+            organizacion: "Facultad de Ciencias, Universidad Nacional Autónoma de México",
+            ubicacion: "Distrito Federal, México",
+            año: 2015
+        },
+        {
+            nombre: "Taller de Modelación Numérica",
+            organizacion: "Facultad de Ciencias, Universidad Nacional Autónoma de México",
+            ubicacion: "Distrito Federal, México",
+            año: 2015
+        },
+        {
+            nombre: "Taller de Geoestadística",
+            organizacion: "Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2014
+        },
+        {
+            nombre: "Teoría de Sistemas y Modelamiento Ambiental",
+            organizacion: "Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2013
+        },
+        {
+            nombre: "Taller Consumo Energético Y Calentamiento Global: Metodología de Diagnóstico a Escala Urbana",
+            organizacion: "Facultad de Ciencias Agronómicas, Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2013
+        },
+        {
+            nombre: "Evaluación de Políticas Públicas",
+            organizacion: "Facultad de Ciencias Agronómicas, Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2012
+        },
+        {
+            nombre: "Percepción Remota y Teledetección Ambiental",
+            organizacion: "Universidad de Chile",
+            ubicacion: "Santiago",
+            año: 2012
+        },
+        {
+            nombre: "Taller de Manejo Integrado de Cuencas",
+            organizacion: "Universidad de Chile",
+            ubicacion: "La Pintana, Santiago",
+            año: 2012
+        },
+
+    ];
+
+    cursos.forEach((curso) => {
+      console.log(`Cursos: ${curso.nombre}, ${curso.organizacion}, ${curso.ubicacion} ${curso.año}`);
+    });
 
     const experienceDetails = [
         {
