2f1af27d971f85219e8c6f0042a635f1715eff11
djwillichile <guillermo.f1990@gmail.com>
2023-07-02 02:08:02 -0400

    Update publications.js

--- script/publications.js
+++ script/publications.js
@@ -1,37 +1,56 @@
 const publicationsDetails = [
     {
+        author: 'Morales-Salinas L., Castellaro, G., Frederiksen, N., Roman-Ososrio, L.F., Neira-Roman, J., Fuentes-Jaque, G., Escobar-Avaria, C. & Morales, F.',
+        title: 'Spatial characterization of climatic variables for Arica-Parinacota and Tarapacá, Chile using topoclimatic analysis',
+        link: 'https://publicaciones.unirioja.es/ojs/index.php/cig/article/view/5473',
+        year: '2023',
+    },
+    {
+        author: 'Galdón-Ruíz, A., Fuentes-Jaque, G., Soto, J. & Morales-Salinas, L.',
+        title: 'A simple method for the estimation of minimum and maximum air temperature monthly mean maps using MODIS images in the region of Murcia, Spain',
+        link: 'https://riunet.upv.es/handle/10251/191687',
+        year: '2023',
+    },
+    {
+        author: 'Morales-Salinas, L., Ortega-Farias, S., Riveros-Burgos, C., Chávez, J. L., Wang, S., Tian, F., Carrasco-Benavides, M., Neira-Román, J., López-Olivari, R. & Fuentes-Jaque, G.',
         title: 'Assessment of atmospheric emissivity models for clear-sky conditions with reanalysis data',
-        link: '#',
+        link: 'https://www.researchsquare.com/article/rs-2294837/v1',
         year: '2022',
     },
     {
+        author: 'Cruz, A., Fuentes, G., Arraztio, D., Morales, L., Tapia Opazo, R., Olivares Pacheco, N., & Gochez, A. M.',
         title: 'Analysis of the spatial distribution of species for HLB and Diaphorina citri in South America through an updated version of the Multi-Model Framework',
-        link: '#',
+        link: 'https://scholar.google.com/scholar?cluster=17986575626395505632&hl=en&oi=scholarr',
         year: '2022',
     },
     {
+        author: 'Aranda, A. C., Rivera-Ruiz, D., Rodríguez-López, L., Pedreros, P., Fuentes-Jaque, G., & Urrutia, R.',
         title: 'Evidence of Climate Change Based on Lake Surface Temperature Trends in South Central Chile.',
-        link: '#',
+        link: 'https://www.mdpi.com/2072-4292/13/22/4535',
         year: '2021',
     },
     {
+        author: 'Montaner-Fernández, D., Morales-Salinas, L., Rodriguez, J. S., Cárdenas-Jirón, L., Huete, A., Fuentes-Jaque, G., Pérez-Martínez, W. & Cabezas, J.',
         title: 'Spatio-Temporal variation of the urban heat island in Santiago, Chile during summers 2005–2017',
-        link: '#',
+        link: 'https://www.mdpi.com/2072-4292/12/20/3345',
         year: '2020',
     },
     {
+        author: 'Contador, T., Gañan, M., Bizama, G., Fuentes-Jaque, G., Morales, L., Rendoll, J., Simoes, F., Kennedy, J., Rozzi, R. & Convey, P.',
         title: 'Assessing distribution shifts and ecophysiological characteristics of the only Antarctic winged midge under climate change scenarios',
-        link: '#',
+        link: 'https://www.nature.com/articles/s41598-020-65571-3',
         year: '2020',
     },
     {
+        author: 'Morales Salinas, L., Cárdenas Jirón, L., Montaner Fernández, D., Fuentes-Jaque, G., Muñoz Aguayo, P., & Neira Román, J.',
         title: 'Modelo espacialmente explícito de estimación de las temperaturas extremas diarias en la Ciudad de Santiago, Chile, usando imágenes MODIS e información meteorológica.',
-        link: '#',
+        link: 'https://repositorio.aemet.es/handle/20.500.11765/9991',
         year: '2018',
     },
     {
+        author: 'Castellaro, G., Morales, L., Rodrigo, P., & Fuentes-Jaque, G.',
         title: 'Carga ganadera y capacidad de carga de los pastizales naturales de la Patagonia chilena: estimación a nivel comunal.',
-        link: '#',
+        link: 'http://revistas.uach.cl/index.php/agrosur/article/view/5870',
         year: '2016',
     },
 ];
